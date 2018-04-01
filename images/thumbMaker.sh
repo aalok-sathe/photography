8306f296b6cb1433ed3da6e5f4d0432e33ee874e
aalok.sathe <aalok.sathe@gmail.com>
2018-03-31 20:53:49 -0400

    make resolution arg '300' 	modified:   thumbMaker.sh

--- images/thumbMaker.sh
+++ images/thumbMaker.sh
@@ -1,6 +1,6 @@
 for i in *.jpg *.JPG *.jpeg; do
     if [ "$i" -nt "./thumbs/$i" ]; then
         echo "$i"
-        convert "$i" -thumbnail 500 "./thumbs/$i";
+        convert "$i" -thumbnail 300 "./thumbs/$i";
     fi
 done;
